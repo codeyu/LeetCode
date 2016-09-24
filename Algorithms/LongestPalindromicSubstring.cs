3c89e0f79cdcb505eaa4e670773b8f51f62ee22f
codeyu <codeyu@gmail.com>
2016-09-24 11:51:10 +0800

    005

--- Algorithms/LongestPalindromicSubstring.cs
+++ Algorithms/LongestPalindromicSubstring.cs
@@ -9,14 +9,35 @@
  * substring.
  ***************************************************************************************/
 
-using System;
 namespace Algorithms
 {
     public class Solution005 
     {
         public static string LongestPalindromicSubstring(string s) 
         {
-            throw new NotImplementedException("TODO");
+            var n = s.Length;
+            if(n == 0){ return ""; }
+            var longest = s.Substring(0, 1);
+            for(var i = 0; i < n - 1; i++)
+            {
+                var s1 = ExpandArouondCenter(s, i, i);
+                if(s1.Length > longest.Length){ longest = s1; }
+                var s2 = ExpandArouondCenter(s, i, i + 1);
+                if(s2.Length > longest.Length) { longest = s2; }
+            }
+            return longest;
+        }
+        private static string ExpandArouondCenter(string s, int left, int right)
+        {
+            var L = left;
+            var R = right;
+            var N = s.Length;
+            while(L >= 0 && R <= N - 1 && s[L] == s[R])
+            {
+                L--;
+                R++;
+            }
+            return s.Substring(L + 1, R - L -1);
         }
     }
 }
