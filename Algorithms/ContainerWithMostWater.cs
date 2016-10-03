791892cdf3f45f42a61894a2543cb21f9459ed0f
codeyu <codeyu@gmail.com>
2016-10-03 21:37:59 +0800

    011

---
+++ Algorithms/ContainerWithMostWater.cs
@@ -0,0 +1,53 @@
+﻿// Source : https://leetcode.com/problems/container-with-most-water/ 
+// Author : codeyu 
+// Date : 10/3/16 
+
+/***************************************************************************************
+*
+* Given 
+* n
+*  non-negative integers 
+* a
+* 1
+* , 
+* a
+* 2
+* , ..., 
+* a
+* n
+* , where each represents a point at coordinate (
+* i
+* , 
+* a
+* i
+* ). 
+* n
+*  vertical lines are drawn such that the two endpoints of line 
+* i
+*  is at (
+* i
+* , 
+* a
+* i
+* ) and (
+* i
+* , 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+* 
+* Note: You may not slant the container.
+* 
+*
+**********************************************************************************/
+
+using System;
+using System.Collections.Generic;
+namespace Algorithms
+{
+public class Solution011 
+{
+    public static int MaxArea(int[] height)
+    {
+        throw new NotImplementedException("TODO");
+    }
+}
+}
+
---
+++ AlgorithmsTest/ContainerWithMostWaterTest.cs
@@ -0,0 +1,15 @@
+﻿using Algorithms;
+using Xunit;
+namespace AlgorithmsTest
+{
+    public class ContainerWithMostWaterTest
+    {
+        [Theory]
+        [InlineData()]
+        public void TestMethod(int[] height, int output)
+        {
+            Assert.Equal(output, Solution011.MaxArea(height));
+        }
+    }
+}
+
