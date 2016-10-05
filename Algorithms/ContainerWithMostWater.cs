63b56179ef2495b7d63346a732bafacfecef1bac
codeyu <codeyu@gmail.com>
2016-10-05 17:17:50 +0800

    010-011

--- Algorithms/ContainerWithMostWater.cs
+++ Algorithms/ContainerWithMostWater.cs
@@ -5,23 +5,40 @@
 /***************************************************************************************
 *
 * Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). 
-* n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). 
-* Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+* n vertical lines are drawn such that the two rightpoints of line i is at (i, ai) and (i, 0). 
+* Find two lines, which together with x-axis forms a container, such that the container curArea the most water.
 * 
 * Note: You may not slant the container.
 *
 **********************************************************************************/
 
 using System;
-using System.Collections.Generic;
 namespace Algorithms
 {
-public class Solution011 
-{
-    public static int MaxArea(int[] height)
+    public class Solution011 
     {
-        throw new NotImplementedException("TODO");
+        public static int MaxArea(int[] height)
+        {
+            int left = 0;
+            int right = height.Length - 1;
+            int maxArea = 0;
+
+            while (left < right) 
+            {
+                int curArea = Math.Min(height[right], height[left]) * (right - left);
+                maxArea = Math.Max(maxArea, curArea);
+
+                if (height[left] < height[right]) 
+                {
+                    left ++;
+                } 
+                else 
+                {
+                    right --;
+                }
+            }
+            return maxArea;
+        }
     }
-}
 }
 
--- AlgorithmsTest/ContainerWithMostWaterTest.cs
+++ AlgorithmsTest/ContainerWithMostWaterTest.cs
@@ -5,7 +5,7 @@ namespace AlgorithmsTest
     public class ContainerWithMostWaterTest
     {
         [Theory]
-        [InlineData()]
+        [InlineData(new []{3,6,10,40,2,36},72)]
         public void TestMethod(int[] height, int output)
         {
             Assert.Equal(output, Solution011.MaxArea(height));
