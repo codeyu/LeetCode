ff36b94b7ea6c1c56d5eeba1760eb99ad852c4e0
codeyu <codeyu@gmail.com>
2016-10-12 23:58:04 +0800

    add treenode

---
+++ Algorithms/Utils/TreeNode.cs
@@ -0,0 +1,13 @@
+namespace Algorithms.Utils
+{
+    public class TreeNode
+    {
+        public int Val{get;set;}
+        public TreeNode Left {get;set;}
+        public TreeNode Right{get;set;}
+        public TreeNode(int x)
+        {
+            Val = x;
+        }
+    }
+}
