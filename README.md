d8b43534ae95251ff3c7c22b66289972c7e40fca
codeyu <codeyu@gmail.com>
2016-10-13 23:24:31 +0800

    028

--- README.md
+++ README.md
@@ -34,3 +34,4 @@ LeetCode
 |025|[Reverse Nodes in k-Group](https://leetcode.com/problems/reverse-nodes-in-k-group/) | [C#](./Algorithms/ReverseNodesink-Group.cs)|Hard|
 |026|[Remove Duplicates from Sorted Array](https://leetcode.com/problems/remove-duplicates-from-sorted-array/) | [C#](./Algorithms/RemoveDuplicatesfromSortedArray.cs)|Easy|
 |027|[Remove Element](https://leetcode.com/problems/remove-element/) | [C#](./Algorithms/RemoveElement.cs)|Easy|
+|028|[Implement strStr](https://leetcode.com/problems/implement-strstr/) | [C#](./Algorithms/ImplementstrStr.cs)|Easy|
--- Scripts/auto.ps1
+++ Scripts/auto.ps1
@@ -33,13 +33,13 @@ if ($IsWindows)
 else{
     $QuestionContent = ($HTML.Content | pup -p 'head meta[name=\"description\"] attr{content}').Split("`n")
     $QuestionTitle = $HTML.Content | pup -p 'div[class=\"question-title clearfix\"] > h3 text{}'
-    $DIFFCULT = ($HTML.Content | pup 'div[class=\"question-info text-info\"] > ul > li:last-child text{}').Split(":")[2].Trim()
+    $DIFFCULT = ($HTML.Content | pup 'div[class=\"question-info text-info\"] > ul > li:nth-child(3) text{}').Split(":")[2].Trim()
     $JSON = $HTML.Content | pup -p 'div[ng-controller=\"AceCtrl as aceCtrl\"] attr{ng-init}'
 }
 $AUTHOR = $command
 $CURRENT_DATE = Get-Date -format F
-$NUM = $QuestionTitle.Split(".")[0].Trim().PadLeft(3).Replace(" ", "0") 
-$TITLE = $QuestionTitle.Split(".")[1].Trim().Replace("(", "_").Replace(")", "")
+$NUM = $QuestionTitle.Split(".")[1].Trim().PadLeft(3).Replace(" ", "0") 
+$TITLE = $QuestionTitle.Split(".")[2].Trim().Replace("(", "").Replace(")", "")
 $FILE = $TITLE.Replace(" ", "") + ".cs"
 $TESTFILE = $TITLE.Replace(" ", "") + "Test.cs"
 if ($IsWindows)
